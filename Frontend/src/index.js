8be932f431967ad209aacb7a65db29866b32f63f
michael <mihai@mandrescu.co>
2017-03-21 07:31:11 +0000

    Initial commit - Scaffolding the app

---
+++ Frontend/src/index.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import promiseMiddleware from 'redux-promise';
+
+import App from './components/app';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = compose(applyMiddleware(thunk, promiseMiddleware))(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.querySelector('.container'));
+
