fc61293ea86f3bf1b8175630570db2b04cf4a346
michael <mihai@mandrescu.co>
2017-03-23 20:29:10 +0000

    Fixes to scaffolding. Now works decently.

--- Scaffolding/build_settings.py
+++ Scaffolding/build_settings.py
@@ -1,15 +1,55 @@
-from Backend.Backend import settings
+import getpass
+import old_settings as settings
 
 import json
 
 
 def get_settings_content():
-    content = ""
-    content += settings.SECRET_KEY
-    content += "\n"
-    content += json.dumps(settings.INSTALLED_APPS + ['rest_framework'])
+    content = "import os" + "\n\n"
+    content += "import getpass" + "\n\n"
+    content += "BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))" + "\n\n"
+    content += "SECRET_KEY = '%s'" % settings.SECRET_KEY + "\n\n"
+    content += "DEBUG = getpass.getuser() == '%s'" % getpass.getuser() + "\n\n"
+    content += "ALLOWED_HOSTS = []" + "\n\n"
+    # ToDo: Add a list of middle ware that can be initialized from here
+    content += "INSTALLED_APPS = " + json.dumps(settings.INSTALLED_APPS + ['rest_framework'], indent=4) + "\n\n"
+    content += "MIDDLEWARE = " + json.dumps(settings.MIDDLEWARE + [], indent=4) + "\n\n"
+    content += "ROOT_URLCONF = 'Backend.urls'" + "\n\n"
+    content += "TEMPLATES = " + json.dumps(settings.TEMPLATES, indent=4) + "\n\n"
+    content += "WSGI_APPLICATION = 'Backend.wsgi.application'" + "\n\n"
 
-    return settings_content
+    content += '''
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, ('db.sqlite3' if getpass.getuser() == '%s' else 'db.sqlite3')),
+        }
+    }
+    '''.strip() % getpass.getuser() + "\n\n"
+
+    content += "AUTH_PASSWORD_VALIDATORS = " + json.dumps(settings.AUTH_PASSWORD_VALIDATORS, indent=4) + "\n\n"
+    content += '''
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.10/howto/static-files/
+
+STATIC_URL = '/static/'
+    '''.strip()
+
+    content += "\n\n"
+    content = content.replace("true", "True")
+
+    return content
 
 with open("settings.py", "w+") as destination:
     settings_content = get_settings_content()
--- django_react_backend.sh
+++ django_react_backend.sh
@@ -5,26 +5,29 @@ CURRED_DIR=$(pwd)
 django-admin startproject Backend
 cd Backend
 django-admin.py startapp backend_app
-python manage.py migrate
 
-# ToDo: Does not seem to work. Find a way to programatically create a super user
+# ToDo: Does not seem to work. Find a way to pragmatically create a super user
 #echo $(cat $CURRED_DIR/password.txt) | python manage.py createsuperuser --username $(whoami) --email $(cat $CURRED_DIR/email.txt)
-
-touch __init__.py
 
 cd $CURRED_DIR
 
 cd Scaffolding
 
-ln -s $CURRED_DIR/Backend backend_link
+cp ../Backend/Backend/settings.py ./old_settings.py
 
-# ToDo: Remove this - Is for debug
-cp backend_link/Backend/settings.py ./old_settings.py
-
-sleep 5
-python -m "from Backend.Backend import settings"
 python build_settings.py
 
-mv settings.py backend_link/Backend/settings.py
+mv settings.py ../Backend/Backend/settings.py
+rm old_settings.py
+
+cd ../Backend
+
+python manage.py migrate
+
+echo
+echo "You will now be prompted to create a superuser."
+echo
+
+python manage.py createsuperuser --username $(whoami) --email $(cat $CURRED_DIR/email.txt)
 
 cd $CURRED_DIR
