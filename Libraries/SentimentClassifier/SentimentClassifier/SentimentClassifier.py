45119bbd81cc80cf63569368f3aa7d9f8a0ec5b7
michael <mihai@mandrescu.co>
2017-03-26 03:06:24 +0100

    Implementing the classifier.
    Added data download script from:
    
    https://archive.ics.uci.edu/ml/machine-learning-databases/00331/

--- Libraries/SentimentClassifier/SentimentClassifier/SentimentClassifier.py
+++ Libraries/SentimentClassifier/SentimentClassifier/SentimentClassifier.py
@@ -1,9 +1,56 @@
+import os
 import nltk
+import pickle
+
+
+def get_data(path, files):
+    data = []
+
+    for file_name in files:
+        with open(os.path.join(path, file_name)) as source:
+            for line in source.readlines():
+                sentence, label = line.split("\t")
+                label = int(label)
+                data.append((sentence, label))
+
+    return data
 
 
 class SentimentClassifier(object):
     def __init__(self):
-        self.test_attribute = "SentimentClassifier works."
+        self.classifier = None
+        self.classifier_path = "/tmp/simple_sentiment_classifier"
+        self.data_source = "/tmp/sentiment_data/sentiment_labelled_sentences"
+
+        # At least one of these must exist
+        # ToDo: Can this be made better?
+        if not os.path.exists(self.data_source) and not os.path.exists(self.classifier_path):
+            raise Exception("No data set or classifier found. Please run the download_data script.")
+
+    def get_classifier(self):
+        # Try to load or train a new classifier
+        try:
+            self._load()
+        except IOError:
+            self._train()
+            self._save()
+
+        if self.classifier is None:
+            raise Exception("Could not get classifier.")
+
+    def _load(self):
+        with open(self.classifier_path, 'rb') as source:
+            self.classifier = pickle.load(source)
+
+    def _save(self):
+        with open('my_classifier.pickle', 'wb') as destination:
+            pickle.dump(self.classifier, destination)
+
+    def _train(self):
+        self.data = get_data(self.data_source,
+                             ["amazon_cells_labelled.txt",
+                              "imdb_labelled.txt",
+                              "yelp_labelled.txt"])
 
     def classify(self, classification_input):
         return {
@@ -24,5 +71,26 @@ class TestSentimentClassifier(unittest.TestCase):
         del self.sc
 
     def test_SentimentClassifier(self):
-        self.assertEquals(self.sc.test_attribute, "SentimentClassifier works.")
         self.assertEquals(self.sc.classify({"text": "test"}), "SentimentClassifier works.")
+
+    def test_get_data(self):
+        result = get_data("/tmp/sentiment_data/sentiment_labelled_sentences",
+                          ["amazon_cells_labelled.txt",
+                           "imdb_labelled.txt",
+                           "yelp_labelled.txt"])
+
+        self.assertEquals(len(result), 3000)
+
+        # Make sure there is 1500 0 labels and 1500 1 labels
+        count_0 = 0
+        count_1 = 0
+        for _, label in result:
+            if label == 0:
+                count_0 += 1
+            elif label == 1:
+                count_1 += 1
+            else:
+                raise Exception("Unknown label found.")
+
+        self.assertEquals(1500, count_0)
+        self.assertEquals(1500, count_1)
---
+++ Libraries/SentimentClassifier/SentimentClassifier/download_data.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+CURRENT_DIRECTORY=$(pwd)
+
+cd /tmp/
+
+mkdir sentiment_data
+
+cd sentiment_data
+
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/00331/sentiment%20labelled%20sentences.zip
+
+unzip sentiment\ labelled\ sentences.zip
+
+mv sentiment\ labelled\ sentences sentiment_labelled_sentences
+
+cd $CURRENT_DIRECTORY
